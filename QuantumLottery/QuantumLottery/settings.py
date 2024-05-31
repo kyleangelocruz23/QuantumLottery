be057fe86af1a6caf269761476c9b53dd0bbfc61
kyle <kyleangelocruz23@gmail.com>
2024-05-31 17:48:42 +0800

    1st checkpoint

--- QuantumLottery/QuantumLottery/settings.py
+++ QuantumLottery/QuantumLottery/settings.py
@@ -117,9 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'home', 'static'),
 ]
 
 # Default primary key field type
--- QuantumLottery/home/views.py
+++ QuantumLottery/home/views.py
@@ -1,7 +1,6 @@
 # home/views.py
 
 from django.shortcuts import render
-from django.http import HttpResponse
 from .qrng_ticket_id import generate_ticket_id, generate_qr_code
 import os
 
@@ -9,11 +8,14 @@ def index(request):
     if request.method == "POST":
         selected_numbers = request.POST.get('selected_numbers')
         ticket_id = generate_ticket_id()
-        qr_code_path = os.path.join('static', 'ticket_qr.png')
+        qr_code_dir = os.path.join('home', 'static')
+        os.makedirs(qr_code_dir, exist_ok=True)  # Ensure the directory exists
+        qr_code_path = os.path.join(qr_code_dir, 'ticket_qr.png')
         generate_qr_code(ticket_id, qr_code_path)
+        qr_code_url = '/static/ticket_qr.png'  # Construct URL for the QR code image
         return render(request, 'home/result.html', {
             'selected_numbers': selected_numbers,
             'ticket_id': ticket_id,
-            'qr_code_path': qr_code_path
+            'qr_code_url': qr_code_url
         })
     return render(request, 'home/index.html')
